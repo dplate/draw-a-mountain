ecbc50fba249d4c7f7de603ab3adc0ef06203c7a
Dirk Plate <email@dplate.de>
2020-04-23 10:55:12 +0200

    Place restaurant on ridge when click on sky

--- src/restaurant/addRestaurant.js
+++ src/restaurant/addRestaurant.js
@@ -48,8 +48,24 @@ const setOpacity = (meshes, opacity) => {
   });
 }
 
+const findNearestTerrain = (terrain, clickPoint) => {
+  const checkPoint = clickPoint.clone();
+  let searchStep = clickPoint.y / 3;
+  let terrainInfo = null;
+  while (searchStep > 0.001 && !(terrainInfo && Math.abs(clickPoint.y - terrainInfo.point.y) < 0.001)) {
+    terrainInfo = terrain.getTerrainInfoAtPoint(checkPoint);
+    if (terrainInfo || checkPoint.y <= 0) {
+      checkPoint.y += searchStep;
+      searchStep /= 3;
+    }
+    checkPoint.y -= searchStep;
+    checkPoint.y = Math.max(checkPoint.y, 0);
+  }
+  return terrainInfo;
+}
+
 const updatePosition = (terrain, supportMesh, backMesh, frontMesh, clickPoint) => {
-  const terrainInfoCenter = terrain.getTerrainInfoAtPoint(clickPoint);
+  const terrainInfoCenter = findNearestTerrain(terrain, clickPoint);
   if (terrainInfoCenter) {
     const {y, supportXOffset, frontXOffset} = getSlopeInfo(terrain, terrainInfoCenter.point);
 
