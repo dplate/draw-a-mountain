4347fd9b0c6fc5f30dd9712261b1b2c47a34a2fb
Dirk Plate <email@dplate.de>
2020-05-09 12:16:46 +0200

    Fix exception when moving out of terrain

--- src/lib/findNearestTerrain.js
+++ src/lib/findNearestTerrain.js
@@ -1,5 +1,7 @@
 export default (terrain, clickPoint) => {
   const checkPoint = clickPoint.clone();
+  checkPoint.x = Math.min(Math.max(0, checkPoint.x), 1);
+  checkPoint.y = Math.max(checkPoint.y, 0);
   let searchStep = clickPoint.y / 3;
   let terrainInfo = null;
   while (searchStep > 0.001 && !(terrainInfo && Math.abs(clickPoint.y - terrainInfo.point.y) < 0.001)) {
