7576700f44379c4623acf9766db6550eefaf6106
Dirk Plate <email@dplate.de>
2020-05-22 15:29:49 +0200

    Add climbing

--- src/paths/hikers/walk.js
+++ src/paths/hikers/walk.js
@@ -4,11 +4,28 @@ import getPersonDirection from "../../lib/getPersonDirection.js";
 import calculateOpticalDistance from "../../lib/calculateOpticalDistance.js";
 import handlePassing from "./handlePassing.js";
 
-const calculateSpeed = (lastStep, nextStep, baseSpeed) => {
-  const slope = (nextStep.point.y - lastStep.point.y) / nextStep.point.distanceTo(lastStep.point);
+const calculateSlope = (lastStep, nextStep) => {
+  return (nextStep.point.y - lastStep.point.y) / nextStep.point.distanceTo(lastStep.point);
+};
+
+const calculateSpeed = (difficulty, slope, baseSpeed) => {
+  if (difficulty === 2) {
+    return baseSpeed * 0.3;
+  }
   const speedFactor = Math.min(Math.max(0.25, 1 - slope), 1.5);
   return baseSpeed * speedFactor;
 };
+
+const isClimbing = (difficulty, slope) => {
+  return difficulty === 2 && Math.abs(slope) > 0.07;
+};
+
+const getDirection = (currentPoint, nextPoint, climbing, slope) => {
+  if (climbing && slope < 0) {
+    return getPersonDirection(nextPoint, currentPoint);
+  }
+  return getPersonDirection(currentPoint, nextPoint)
+}
 
 export default (terrain, hikers, hiker, elapsedTime) => {
   const lastStepIndex = Math.floor(hiker.data.progress);
@@ -18,11 +35,14 @@ export default (terrain, hikers, hiker, elapsedTime) => {
   if (nextStep) {
     handlePassing(terrain, hikers, hiker);
     const opticalDistance = calculateOpticalDistance(lastStep.point, nextStep.point);
+    const slope = calculateSlope(lastStep, nextStep);
+    const difficulty = hiker.data.path.difficulty;
+    const climbing = isClimbing(difficulty, slope);
 
-    hiker.person.animation = 'walking';
+    hiker.person.animation = climbing ? 'climbing' : 'walking';
     hiker.person.position.lerpVectors(lastStep.point, nextStep.point, hiker.data.progress % 1);
-    hiker.person.direction = getPersonDirection(hiker.person.position, nextStep.point);
-    hiker.person.speed = calculateSpeed(lastStep, nextStep, hiker.person.baseSpeed);
+    hiker.person.direction = getDirection(hiker.person.position, nextStep.point, climbing, slope);
+    hiker.person.speed = calculateSpeed(difficulty, slope, hiker.person.baseSpeed);
 
     hiker.data.progress += elapsedTime * hiker.person.speed * (0.005 / opticalDistance);
     return 'walk';
--- src/persons/animatePerson.js
+++ src/persons/animatePerson.js
@@ -12,6 +12,20 @@ const animateWalking = (person, elapsedTime) => {
   person.rightArm.angle = -armAngle;
 };
 
+const animateClimbing = (person, elapsedTime) => {
+  person.cycle = (person.cycle + elapsedTime * person.speed) % 1;
+
+  const averageLegAngle = -Math.PI / 10;
+  const legAngle = Math.sin(person.cycle * 2 * Math.PI) * Math.PI / 10;
+  person.leftLeg.angle = averageLegAngle + legAngle;
+  person.rightLeg.angle = averageLegAngle - legAngle;
+
+  const averageArmAngle = -Math.PI / 4;
+  const armAngle = +Math.sin(person.cycle * 2 * Math.PI) * Math.PI / 10;
+  person.leftArm.angle = averageArmAngle + armAngle;
+  person.rightArm.angle = averageArmAngle - armAngle;
+};
+
 const animateStanding = (person) => {
   person.cycle = 0;
   person.leftLeg.angle = 0;
@@ -24,6 +38,9 @@ export default async (person, elapsedTime) => {
   switch (person.animation) {
     case "walking":
       animateWalking(person, elapsedTime);
+      break;
+    case "climbing":
+      animateClimbing(person, elapsedTime);
       break;
     case "standing":
       animateStanding(person);
