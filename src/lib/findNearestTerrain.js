5312b9f6d09efb48971cbbc7b8c0e2699f46af5b
Dirk Plate <email@dplate.de>
2020-05-24 12:14:37 +0200

    Fix exceptions when drawing path near bottom

--- src/lib/findNearestTerrain.js
+++ src/lib/findNearestTerrain.js
@@ -1,8 +1,10 @@
+const checkPoint = new THREE.Vector3();
+
 export default (terrain, clickPoint) => {
-  const checkPoint = clickPoint.clone();
+  checkPoint.copy(clickPoint);
   checkPoint.x = Math.min(Math.max(0, checkPoint.x), 1);
   checkPoint.y = Math.max(checkPoint.y, 0);
-  let searchStep = clickPoint.y / 3;
+  let searchStep = Math.max(clickPoint.y / 3, 0.002);
   let terrainInfo = null;
   while (searchStep > 0.001 && !(terrainInfo && Math.abs(clickPoint.y - terrainInfo.point.y) < 0.001)) {
     terrainInfo = terrain.getTerrainInfoAtPoint(checkPoint);
