7aa91a7834a13b2193171f5030c58987e335672d
Dirk Plate <email@dplate.de>
2020-05-29 19:01:38 +0200

    Fix no terrainInfo found

--- src/lib/findNearestTerrain.js
+++ src/lib/findNearestTerrain.js
@@ -1,12 +1,13 @@
 const checkPoint = new THREE.Vector3();
 
 export default (terrain, clickPoint) => {
+  const optimalY = Math.max(clickPoint.y, 0);
   checkPoint.copy(clickPoint);
   checkPoint.x = Math.min(Math.max(0, checkPoint.x), 1);
   checkPoint.y = Math.max(checkPoint.y, 0);
-  let searchStep = Math.max(clickPoint.y / 3, 0.002);
+  let searchStep = Math.max(checkPoint.y / 3, 0.004);
   let terrainInfo = null;
-  while (searchStep > 0.001 && !(terrainInfo && Math.abs(clickPoint.y - terrainInfo.point.y) < 0.001)) {
+  while (searchStep > 0.001 && !(terrainInfo && Math.abs(optimalY - terrainInfo.point.y) < 0.001)) {
     terrainInfo = terrain.getTerrainInfoAtPoint(checkPoint);
     if (terrainInfo || checkPoint.y <= 0) {
       checkPoint.y += searchStep;
