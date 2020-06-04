446ed23bd3a6e3935e1374df240d0641a050a2bd
Dirk Plate <email@dplate.de>
2020-06-04 10:40:56 +0200

    Fix walking to a point on low framerates

--- src/lib/walkToPoint.js
+++ src/lib/walkToPoint.js
@@ -4,15 +4,16 @@ const walkVector = new THREE.Vector3();
 
 export default (person, endPoint, elapsedTime) => {
   walkVector.subVectors(endPoint, person.position);
-  if (walkVector.length() > 0.0002) {
+  const distance = walkVector.length();
+  if (distance > 0.0002) {
     walkVector.normalize()
     walkVector.multiplyScalar(
-      elapsedTime * 0.005 * person.baseSpeed
+      Math.min(elapsedTime * 0.005 * person.baseSpeed, distance)
     );
     person.animation = 'walking';
+    person.direction = getPersonDirection(person.position, endPoint);
     person.position.add(walkVector);
     person.position.z = endPoint.z;
-    person.direction = getPersonDirection(person.position, endPoint);
     person.speed = person.baseSpeed;
     return false;
   } else {
