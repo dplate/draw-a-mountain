6a9bdec5729beecd4f8552287209b4056df5e4a6
Dirk Plate <email@dplate.de>
2020-06-05 11:05:42 +0200

    Use InstancedMesh for rocks

--- src/terrain/createRocks.js
+++ src/terrain/createRocks.js
@@ -1,18 +1,18 @@
-import loadSvg from "../lib/loadSvg.js";
+import createInstancedObjectFromSvg from "../lib/createInstancedObjectFromSvg.js";
 
 const MIN_ROCK_SLOPE = 0.8;
 
 export default async (scene, terrainMesh) => {
   const meshes = [
-    await loadSvg('rocks/lime'),
-    await loadSvg('rocks/basalt')
+    await createInstancedObjectFromSvg(scene, 'rocks/lime'),
+    await createInstancedObjectFromSvg(scene, 'rocks/basalt')
   ]
   const terrainGeometry = new THREE.Geometry().fromBufferGeometry(terrainMesh.geometry);
   const vertices = terrainGeometry.vertices;
   const rocks = [];
   terrainGeometry.faces.forEach(face => {
     if (face.normal.y < MIN_ROCK_SLOPE && Math.random() * MIN_ROCK_SLOPE > face.normal.y && Math.random() < 0.5) {
-      const rock = meshes[Math.floor(Math.random() * meshes.length)].clone();
+      const rock = meshes[Math.floor(Math.random() * meshes.length)].addInstance();
       const scale = 0.03 - Math.random() * face.normal.y / MIN_ROCK_SLOPE * 0.02;
       rock.scale.x = scale;
       rock.scale.y = 0;
@@ -24,14 +24,14 @@ export default async (scene, terrainMesh) => {
         baseY: rock.position.y
       };
       rocks.push(rock);
-      scene.add(rock);
     }
   });
   terrainGeometry.dispose();
   return movement => {
     rocks.forEach(rock => {
       rock.scale.y = rock.userData.scale * movement;
-      rock.position.y = rock.userData.baseY + rock.userData.scale * 1.5 * movement
+      rock.position.y = rock.userData.baseY + rock.userData.scale * 1.5 * movement;
+      rock.update();
     });
   }
 };
